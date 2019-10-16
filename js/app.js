c31404751bc8b0ed08e5679ce3a3065aa4b05a6a
Yodsakorn009 <meanyok.mg@gmail.com>
2019-10-16 10:41:14 +0700

    v1.16

--- js/app.js
+++ js/app.js
@@ -1,91 +1,98 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCUiwtRUD9RdLqH4KmStlHrccUiqYynaZQ",
-    authDomain: "dealfood-ae8f9.firebaseapp.com",
-    databaseURL: "https://dealfood-ae8f9.firebaseio.com",
-    projectId: "dealfood-ae8f9",
-    storageBucket: "dealfood-ae8f9.appspot.com",
-    messagingSenderId: "543852763810",
-    appId: "1:543852763810:web:28e5fb536995261bcd55fb",
-    measurementId: "G-NKTZL9F88H"
-  };
+  apiKey: "AIzaSyD-XI0IiRFoNNXmuWeES-pS4Y7Kzygv-l8",
+  authDomain: "dealfood-ed34b.firebaseapp.com",
+  databaseURL: "https://dealfood-ed34b.firebaseio.com",
+  projectId: "dealfood-ed34b",
+  storageBucket: "dealfood-ed34b.appspot.com",
+  messagingSenderId: "934386896591",
+  appId: "1:934386896591:web:f397ee2388552fbe4371ba"
+};
+firebase.initializeApp(firebaseConfig);
 
-  firebase.initializeApp(firebaseConfig);
+//  var db = firebase.firestore();
 
-   var db = firebase.firestore();
 
-  // ดูสถานะการ login
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
 
-      var email = user.email;
-      console.log(email + "signed in");
-  
-    } else {
-      console.log("sign out");
-   
-    }
-  });
-  
+// ดูสถานะการ login
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+
+    var email = user.email;
+    console.log(email + "signed in");
+
+  } else {
+    console.log("sign out");
+
+  }
+});
+
 
 document.addEventListener('init', function (event) {
-    var page = event.target;
-    console.log(page.id);
+  var page = event.target;
+  console.log(page.id);
 
-    if (page.id === "tabbar") {
-        //Code for tabbar
-        $("#menubtn").click(function () {
-            var menu = document.getElementById('menu');
-            menu.open();
-        });
-    }
+  if (page.id === "tabbar") {
+    //Code for tabbar
+    $("#menubtn").click(function () {
+      var menu = document.getElementById('menu');
+      menu.open();
+    });
+  }
 
-    if (page.id === "sidemenu") {
-        //Code for sidemenu
+  if (page.id === "sidemenu") {
+    //Code for sidemenu
 
-        $("#logout").click(function () {
-            //firebase sign out
-            firebase.auth().signOut().then(function() {
-              // Sign-out successful.
-              $("#content")[0].load("login.html");  
-              $("#sidemenu")[0].close();   
-            }).catch(function(error) {
-              // An error happened.
-              console.log(error.message);
-            });
-          });
+    $("#logout").click(function () {
+      //firebase sign out
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        var content = document.getElementById('content');
+        var menu = document.getElementById('menu');
+        content.load('login.html')
+          .then(menu.close.bind(menu));
+        console.log("logout");
         
+      }).catch(function (error) {
+        // An error happened.
+        console.log(error.message);
+      });
+    });
+
     $("#login").click(function () {
-        $("#content")[0].load("login.html");  
-        $("#sidemenu")[0].close();   
-      }); 
+      var content = document.getElementById('content');
+      var menu = document.getElementById('menu');
+      content.load('login.html')
+        .then(menu.close.bind(menu));
+     
+    });
 
-        $("#home").click(function () {
-            var content = document.getElementById('content');
-            var menu = document.getElementById('menu');
-            content.load('index.html')            
-                .then(menu.close.bind(menu));
-                $("#sidemenu")[0].close();   
-        });
-  
-    }
-    if (page.id === 'loginPage') {
-        console.log("loginPage");
-    
-        $("#signinbtn").click(function(){
-          var username = $("#username").val();
-          var password = $("#password").val();
-          firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
-    
-            console.log(error.message);
-          });
-    
-        })
-    
-        $("#backhomebtn").click(function () {
-          $("#content")[0].load("home.html");      
-        });
-      }
+  }
+  if (page.id === 'loginPage') {
+    console.log("loginPage");
 
-    
+    $("#signinbtn").click(function () {
+      var username = $("#username").val();
+      var password = $("#password").val();
+      firebase.auth().signInWithEmailAndPassword(username, password)
+
+      content.load('Home.html')
+        .then(menu.close.bind(menu))
+
+
+        .catch(function (error) {
+
+          console.log(error.message);
+
+
+        });
+
+    })
+
+    $("#backhomebtn").click(function () {
+      $("#content")[0].load("home.html");
+    });
+  }
+
+
 
 });
