49b78f2ec115d7690f3343ce680b50f48d230e8a
Yodsakorn009 <meanyok.mg@gmail.com>
2019-10-18 09:33:31 +0700

    เกือบเสร็จ

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ document.addEventListener('init', function (event) {
 
 
 
-  
+
   if (page.id === "more") {
     //Code for tabbar
     $("#menubtn").click(function () {
@@ -41,14 +41,14 @@ document.addEventListener('init', function (event) {
       menu.open();
     });
 
-     
-    
-        $("#mf").empty();
-        db.collection("home").get().then((querySnapshot) => {
-          var item
-          querySnapshot.forEach((doc) => {
-              
-          item = `  
+
+
+    $("#mf").empty();
+    db.collection("home").get().then((querySnapshot) => {
+      var item
+      querySnapshot.forEach((doc) => {
+
+        item = `  
           <ons-card id="foodc">
           <center>
             <ons-toolbar-button  onclick="validate5()"><img src="${doc.data().photoUrl}" alt="Onsen UI"
@@ -58,13 +58,11 @@ document.addEventListener('init', function (event) {
              </center>
              </ons-card>
            `
-             
-           
-      $("#mf").append(item);
-          });
-        
-        });    
-    
+        $("#mf").append(item);
+      });
+
+    });
+
   }
 
   if (page.id === "home") {
@@ -78,32 +76,33 @@ document.addEventListener('init', function (event) {
       var menu = document.getElementById('menu');
       content.load('more.html')
         .then(menu.close.bind(menu));
-     
+
     });
 
 
-    $("#carousel").empty();
-    db.collection("recommended").get().then((querySnapshot) => {
+    $("#carose").empty();
+    db.collection("home").get().then((querySnapshot) => {
+      var item
       querySnapshot.forEach((doc) => {
-          
-        var item = `
-        <ons-carousel-item modifier="nodivider" id="${doc.data().id}" class="recomended_item">
-            <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
-            </div>
-            <div class="recomended_item_title" id="item1_name">${doc.data().name}</div>
-        </ons-carousel-item>`
-        
-        $("#carousel").append(item);
 
+      item = `<ons-carousel-item modifier="nodivider" id="${doc.data().id}" class="recomended_item">
+        <ons-toolbar-button  onclick="validate5()"><img src="${doc.data().photoUrl}" alt="Onsen UI"
+                    style="width: 90%"></ons-toolbar-button>
+                    <div>${doc.data().name}</div>
+        </ons-carousel-item>`
+
+        $("#carosel").append(item);
+
+       
       });
-    });     
-    
-        $("#mf").empty();
-        db.collection("home").get().then((querySnapshot) => {
-          var item
-          querySnapshot.forEach((doc) => {
-              
-          item = `  <ons-col  width="50%">
+    });
+
+    $("#mf").empty();
+    db.collection("home").get().then((querySnapshot) => {
+      var item
+      querySnapshot.forEach((doc) => {
+
+        item = `  <ons-col  width="50%">
           <ons-card id="foodc">
           <center>
             <ons-toolbar-button  onclick="validate5()"><img src="${doc.data().photoUrl}" alt="Onsen UI"
@@ -113,53 +112,53 @@ document.addEventListener('init', function (event) {
              </center>
              </ons-card>
              </ons-col>`
-             
-           
-      $("#mf").append(item);
-          });
-        
-        });    
-    
+
+
+        $("#mf").append(item);
+      });
+
+    });
+
   }
-    
-  
-   
+
+
+
 
   if (page.id === "sidemenu") {
     //Code for sidemenu
 
     $("#logout").click(function () {
       //firebase sign out
-      firebase.auth().signOut().then(function() {
+      firebase.auth().signOut().then(function () {
         // Sign-out successful.
         var content = document.getElementById('content');
         var menu = document.getElementById('menu');
         content.load('login.html')
           .then(menu.close.bind(menu));
         console.log("logout");
-        
+
       }).catch(function (error) {
         // An error happened.
         console.log(error.message);
       });
     });
 
-    
+
     $("#cls").click(function () {
       var content = document.getElementById('content');
       var menu = document.getElementById('menu');
       content.load('home.html')
         .then(menu.close.bind(menu));
-     
+
     });
- 
+
     //Code for tabbar
     $("#more").click(function () {
       var content = document.getElementById('content');
       var menu = document.getElementById('menu');
       content.load('more.html')
         .then(menu.close.bind(menu));
-     
+
     });
 
     $("#login").click(function () {
@@ -167,28 +166,48 @@ document.addEventListener('init', function (event) {
       var menu = document.getElementById('menu');
       content.load('login.html')
         .then(menu.close.bind(menu));
-     
+
     });
 
   }
   if (page.id === 'loginPage') {
-    
+
+    $("#gglog").click(function () {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        content.load('Home.html')
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    })
 
     $("#signinbtn").click(function () {
       var username = $("#username").val();
       var password = $("#password").val();
-      firebase.auth().signInWithEmailAndPassword(username, password).then(function(result){
+      firebase.auth().signInWithEmailAndPassword(username, password).then(function (result) {
 
-      content.load('Home.html')
-        .then(menu.close.bind(menu))
-
-
-       }) .catch(function (error) {
-          
-          alert('Try Again');
+        content.load('Home.html')
+          .then(menu.close.bind(menu))
 
 
-        });
+      }).catch(function (error) {
+
+        alert('Try Again');
+
+
+      });
 
     })
     $("#signup").click(function () {
@@ -197,39 +216,51 @@ document.addEventListener('init', function (event) {
 
 
 
-      $("#backhomebtn").click(function () {
+    $("#backhomebtn").click(function () {
       $("#content")[0].load("home.html");
     });
-    
+
   }
   if (page.id === 'signupPage') {
-  
+
     $("#createbtn").click(function () {
+
       var email = document.getElementById('email').value;
       var password = document.getElementById('password').value;
-              firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              
-              if(errorCode === 'auth/weak-password'){
-                alert(errorMessage);
-                          
-              }              
-              else if(errorCode == 'auth/invalid-email'){
-                alert(errorMessage);
-              }
-              console.log(error);
-              
-          }
-          
-          );
-          
-       
-        });
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function (result) {
 
-        $("#backhomebtn").click(function () {
-          $("#content")[0].load("login.html");
-        });
+        content.load('Home.html')
+          .then(menu.close.bind(menu))
+
+
+      })
+      
+      
+      
+      
+      
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === 'auth/weak-password') {
+          alert(errorMessage);
+
+        }
+        else if (errorCode == 'auth/invalid-email') {
+          alert(errorMessage);
+        }
+        console.log(error);
+
+      }
+      );
+
+
+    });
+
+    $("#backhomebtn").click(function () {
+      $("#content")[0].load("login.html");
+    });
   }
 });
